55a031ec7f5dbe6f25a91b553892b3c9009301bb
brian_Chan_Lee <brianlee1914@me.com>
2026-05-15 11:23:48 +0900

    ci: deploy vite app to github pages via actions

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { logger } from '../utils/logger'
 import i18n from '../i18n'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vx_web_demo/',
   plugins: [vue()],
   resolve: {
     alias: {
